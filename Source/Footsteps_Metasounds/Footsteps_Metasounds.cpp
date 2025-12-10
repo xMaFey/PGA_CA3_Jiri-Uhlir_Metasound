9096c291e211bb2e34f21bddceccf257f5d1370d
xMaFey <uhlir2002@seznam.cz>
2025-12-10 13:25:50 +0000

    Submission Commit

---
+++ Source/Footsteps_Metasounds/Footsteps_Metasounds.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Footsteps_Metasounds.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Footsteps_Metasounds, "Footsteps_Metasounds" );
